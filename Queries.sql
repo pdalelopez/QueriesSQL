267ba65632562a1f73fdce5407389d949252a3b8
IT PC <>
2020-02-06 13:15:56 +0100

    queries 3 and 4

--- Queries.sql
+++ Queries.sql
@@ -1,2 +1,4 @@
 select * from usairlineflights2.flights;
 select distinct origin, avg(ArrDelay), avg(DepDelay) from usairlineflights2.flights group by origin;
+select origin, colYear, colMonth, avg(ArrDelay) from usairlineflights2.flights group by origin, colYear, colMonth order by origin,colYear,colMonth;
+select city, colYear, colMonth, avg(ArrDelay) from usairports inner join flights on (usairports.IATA=flights.origin) group by city , colYear, colMonth order by city,colYear,colMonth;
