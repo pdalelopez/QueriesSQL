91ab7db5e8b7976778b0d29ff80d61d03b4146c1
IT PC <>
2020-02-07 10:56:30 +0100

    queries 5 and 6 ready

--- Queries.sql
+++ Queries.sql
@@ -1,4 +1,6 @@
 select * from usairlineflights2.flights;
-select distinct origin, avg(ArrDelay), avg(DepDelay) from usairlineflights2.flights group by origin;
+select  origin, avg(ArrDelay), avg(DepDelay) from usairlineflights2.flights group by origin;
 select origin, colYear, colMonth, avg(ArrDelay) from usairlineflights2.flights group by origin, colYear, colMonth order by origin,colYear,colMonth;
 select city, colYear, colMonth, avg(ArrDelay) from usairports inner join flights on (usairports.IATA=flights.origin) group by city , colYear, colMonth order by city,colYear,colMonth;
+select UniqueCarrier, colYear, colMonth, SUM(Cancelled) as Total_Cancelled from flights group by UniqueCarrier, colYear, colMonth order by Total_Cancelled desc;
+select TailNum, SUM(Distance) as Total_Distance from flights where TailNum not like '' group by TailNum order by Total_Distance desc limit 10;
